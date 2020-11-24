6e2d423ed936e5b61cbee42111730730e88d303b
ikbalkazanc <60016655+ikbalkazanc@users.noreply.github.com>
2020-11-24 21:59:39 +0300

    Add project files.

---
+++ Course.IdentityServer.AuthServer/Config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace Course.IdentityServer.AuthServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GerApiResoruce()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("resource_Api1"){Scopes = {"api1.read","api1.write","api1.update"}},
+                new ApiResource("resource_api2"){Scopes = {"api2.read","api2.write","api2.update"}}
+            };
+        }
+        public static IEnumerable<ApiScope> GerApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("api1.read","read permission for API 1"),
+                new ApiScope("api1.write","write permission for API 1"),
+                new ApiScope("api1.update","update permission for API 1"),
+                new ApiScope("api2.read","read permission for API 2"),
+                new ApiScope("api2.write","write permission for API 2"),
+                new ApiScope("api2.update","update permission for API 2"),
+            };
+        }
+    }
+}
